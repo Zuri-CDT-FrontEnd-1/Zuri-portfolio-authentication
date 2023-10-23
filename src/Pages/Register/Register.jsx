82c7151b816785cd326e0b4112c273937cfecdc0
Qaasim Umar <omayr010@gmail.com>
2023-10-23 17:11:45 +0100

    added login ui

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,68 +1,88 @@
-import React, { useState, useEffect } from 'react';
-// import DisplayData from './DisplayData';
+import React from 'react'
+import './Register.css'
+import login from '../../assets/login.png'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+// import OAuth from 'react-oauth'
+
 
 function Register() {
-  const [email, setEmail] = useState(localStorage.getItem('email') || '');
-  const [password, setPassword] = useState(localStorage.getItem('password') || '');
-  const [image, setImage] = useState(null);
 
-  useEffect(() => {
-    // Retrieve user data from localStorage when the component mounts.
-    const userData = localStorage.getItem('registeredUser');
+const [username, setUsername] = useState('')  
+const [password, setPassword] = useState('')  
 
-    if (userData) {
-      const user = JSON.parse(userData);
-      setEmail(user.email);
-      setPassword(user.password);
-      const imageFileName = user.image;
-      if (imageFileName) {
-        setImage(imageFileName);
-      }
-    }
-  }, []);
+const handleUsernameChange = (e) =>{
+setUsername(e.target.value)
+}
+const handlePasswordChange = (e) =>{
+  setPassword(e.target.value)
+  }
 
-  const handleUpdate = () => {
-    // Update the user data in localStorage with the edited information.
-    const user = {
-      email,
-      password,
-      image: image ? image : null,
-    };
+const handleSubmit = (e) => {
+  e.preventDefault();
 
-    localStorage.setItem('registeredUser', JSON.stringify(user));
+  console.log(username)
+  // Reset form after submission
+  // setPassword('');
+  // setUsername('');
+}
 
-      // Clear the input fields after saving the data
-      setEmail('');
-      setPassword('');
-      setImage(null);
-  };
 
   return (
     <div>
-      <h2>Profile</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => setImage(e.target.files[0])}
-      />
-      <button onClick={handleUpdate}>Update Profile</button>
 
-      {/* Pass data to DisplayData component */}
-      {/*<DisplayData email={email} password={password} image={image} />*/}
+      <div className="relative">
+        <div className="login-container-register"></div>
+        <div className="login-background-register"></div>
+      </div>
+
+
+
+      <div className="login-card-register">
+        <div className="pics-container">
+          <img src={login} className='login-img' alt="" />
+        </div>
+
+        <h2 className='login-heading'>Regsiter Now</h2>
+
+        <form action="" onSubmit={handleSubmit}>
+
+        <div className='flex'>
+            <label htmlFor="fullname" className='form-label'>Fullname*</label>
+            <input type="text" placeholder="Enter your Fullname" name="fullname" class="fullname"  onChange={handleSubmit} required=""></input>
+          </div>
+
+          <div className='flex'>
+            <label htmlFor="username" className='form-label'>Username*</label>
+            <input type="text" placeholder="Enter your Username" name="username" class="username" value={username} onChange={handleSubmit} required=""></input>
+          </div>
+
+          <div className='flex'>
+            <label htmlFor="password" className='form-label'>Password*</label>
+            <input type="text" placeholder="Enter your password" name="username" value={password} onChange={handleSubmit} class="username" required=""></input>
+          </div>
+
+          <label class="checkbox-container">Remember Me
+            <input type="checkbox" />
+            <span class="checkmark"></span>
+          </label>
+
+          <button className='login-button' type='submit' >Register</button>
+
+        </form>
+
+        <div className='flex-row'>
+         <Link to='/login' className='link'><p className='blue'>Already have an account ?</p></Link> 
+          <p className='blue'>Forget password</p>
+        </div>
+
+      </div>
+
+      <div className="footer">
+        <p className='footer-text'>Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Register;
+export default Register
